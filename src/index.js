8784f13149323e6b6489e915f533caad07963666
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-15 16:15:58 -0400

    tutorial notes camelCaseAttributes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//attributes below should actually be javaScript as reacdom.render renders down to javaScript
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
